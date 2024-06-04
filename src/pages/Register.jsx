596c706c8aa284fd071727b1fa61b52edcdbdf7e
KrolLew23 <aniabandola@gmail.com>
2024-06-04 02:26:08 +0200

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
 import { register } from '../services/api';
 import { TextField, Button, Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom'; // Importujemy useNavigate
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
-  const navigate = useNavigate(); // Inicjalizujemy navigate
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: ''
   });
+  const [passwordError, setPasswordError] = useState('');
+  const [emailError, setEmailError] = useState('');
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    if (e.target.name === 'password' && e.target.value.length < 7) {
+      setPasswordError('Password must be at least 7 characters long');
+    } else {
+      setPasswordError('');
+    }
+    // Jeśli zmieniamy adres e-mail, usuwamy komunikat błędu e-maila
+    if (e.target.name === 'email') {
+      setEmailError('');
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -20,9 +31,13 @@ const Register = () => {
     try {
       await register(formData);
       alert('Registration successful!');
-      navigate('/login'); // Po udanej rejestracji przekierowujemy użytkownika do strony logowania
+      navigate('/login');
     } catch (error) {
-      alert('Registration failed!');
+      if (error.response && error.response.data && error.response.data.message === 'Email address is already in use') {
+        setEmailError('Email address is already in use');
+      } else {
+        setEmailError('Email address is already in use');
+      }
     }
   };
 
@@ -31,8 +46,8 @@ const Register = () => {
       <Typography variant="h4" component="h1" gutterBottom>Register</Typography>
       <form onSubmit={handleSubmit}>
         <TextField label="Name" name="name" fullWidth margin="normal" onChange={handleChange} required />
-        <TextField label="Email" name="email" fullWidth margin="normal" onChange={handleChange} required />
-        <TextField label="Password" name="password" type="password" fullWidth margin="normal" onChange={handleChange} required />
+        <TextField label="Email" name="email" fullWidth margin="normal" onChange={handleChange} required error={Boolean(emailError)} helperText={emailError} />
+        <TextField label="Password" name="password" type="password" fullWidth margin="normal" onChange={handleChange} required error={Boolean(passwordError)} helperText={passwordError} />
         <Button type="submit" variant="contained" color="primary" fullWidth>Register</Button>
       </form>
     </div>
